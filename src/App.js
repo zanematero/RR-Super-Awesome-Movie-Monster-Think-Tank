d160b72544ef1cabde38c8b95b2e7c876d75935a
Salman Malik <salmanmalik@Salmans-MBP.attlocal.net>
2021-07-25 16:12:45 -0500

    cleaned up starter code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import AboutUs from './AboutUs'
-import Packages from './Packages'
+import AboutUs from './components/AboutUs'
+import Packages from './components/Packages'
 
 function App() {
   return (
