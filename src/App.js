3703f4e1e311c3adf6ac7cd28de1a3045522d6d7
Zane Matero <materozane@gmail.com>
2023-03-20 00:40:43 -0700

    added suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
 
 function App() {
+  const AboutUs = React.lazy(() => import('./AboutUs'))
+  const Packages = React.lazy(() => import('./Packages'))
+
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<h1>Loading...</h1>}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
